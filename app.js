b23c8a84399da30eeb4a8b9865e4a58925fdcd78
TheMole3 <hugomelo04@gmail.com>
2022-01-23 17:29:49 +0100

    Commit

--- app.js
+++ app.js
@@ -10,11 +10,112 @@ const db = mongojs(config.dbConnect, ['cngMat']); // Import database TheMole and
 
 let cngMat = require('./cngmat.js')(app, db, config); // Start cngmat.js
 
-//cngMat.loadFoodFromSource()
-let main = async ()  => {
-    cngMat.getAdjusted((food, today) => {
-        console.log(food, today)
-    })
-}
+app.use(express.json())
 
-main()
+/*
+    Melo Auth
+*/
+const cookieParser = require("cookie-parser");
+app.use(cookieParser())
+if(config.auth) app.use(require("/home/melo/Servers/Melo.se/meloAuth/meloAuth.js").authenticateToken)
+
+
+/*
+    Program download
+*/
+app.get("/", (req, res) => { // Get available weeks
+    res.sendFile(__dirname + "/client/index.html")
+})
+
+
+/* 
+    API
+*/
+app.get("/getDate", (req, res) => { // Get date from DB ?date=2022-01-23
+    cngMat.getFromDate(req.query.date, (food) => { // Get the db from mongodb
+        res.send(food)
+    })
+})
+app.get("/getAdjusted", (req, res) => { // Get adjusted from DB
+    cngMat.getAdjusted((food) => { // Get the db from mongodb
+        res.send(food)
+    })
+})
+
+app.get("/getAvailableWeeks", (req, res) => { // Get available weeks
+    cngMat.getAvailableWeeks((weeks) => { // Get the db from mongodb
+        res.send(weeks)
+    })
+})
+app.get("/getWeek", (req, res) => { // Get week from DB ?year=2020&week=45
+    cngMat.getWeek({year: req.query.year, week: req.query.week}, (week) => { // Get the db from mongodb
+        res.send(week)
+    })
+})
+
+
+/*
+    Admin
+*/
+app.get('/admin', (req, res) => {
+    if(config.auth) {
+		if(!req.user) return res.redirect("https://login.melo.se/?redirect=https://melo.se/cngmat/admin/") // Om man inte är inloggad, skicka personen till att logga in
+		if(!req.user.scopes.includes("cngMatAdmin")) return res.sendStatus(403) // Om man inte har rätt scope skicka att personen inte är behörig
+	}
+
+	res.sendFile(__dirname + '/client/admin/admin.html')
+})
+app.get("/admin/update", (req, res) => { // Sätt en dags mat till något
+    if(config.auth && !req.user) return res.sendStatus(401) // Om man inte är inloggad, skicka personen till att logga in
+    if(config.auth && !req.user.scopes.includes("cngMatAdmin")) return res.sendStatus(403) // Om man inte har rätt scope skicka att personen inte är behörig
+    cngMat.loadFoodFromSource((status) => {
+        res.sendStatus(status)
+    })
+})
+app.post("/admin/set", (req, res) => { // Sätt en dags mat till något
+    if(config.auth && !req.user) return res.sendStatus(401) // Om man inte är inloggad, skicka personen till att logga in
+    if(config.auth && !req.user.scopes.includes("cngMatAdmin")) return res.sendStatus(403) // Om man inte har rätt scope skicka att personen inte är behörig
+    cngMat.admin.setWeekObject(req.body,
+        (code)=> {
+            res.sendStatus(code)
+        }
+    )
+})
+app.post("/admin/delete", (req, res) => { // Sätt en dags mat till något
+    if(config.auth && !req.user) return res.sendStatus(401) // Om man inte är inloggad, skicka personen till att logga in
+    if(config.auth && !req.user.scopes.includes("cngMatAdmin")) return res.sendStatus(403) // Om man inte har rätt scope skicka att personen inte är behörig
+    cngMat.admin.deleteWeekObject(req.body,
+        (code)=> {
+            res.sendStatus(code)
+        }
+    )
+})
+
+
+/* 
+    Deprecation warning
+*/
+app.get("/getdata", (req, res) => {
+    res.send(`
+    <tile>
+        <visual version="2">
+            <binding template="TileSquare310x310TextList03">
+                <text id="1">Support har upphört</text>
+                <text id="2" hint-wrap="true">Support för denna version av CNGMAT har upphört, ladda ner en ny version på https://melo.se/cngmat/</text>
+            </binding>
+            <binding template="TileWide310x150Text09">
+                <text id="1">Support har upphört</text>
+                <text id="2" hint-wrap="true">Support för denna version av CNGMAT har upphört, ladda ner en ny version på https://melo.se/cngmat/</text>
+            </binding>
+            <binding template="TileMedium">
+                <text id="1" hint-wrap="true">Högerklicka och ändra storleken till bred för att se info</text>
+            </binding>
+        </visual>
+    </tile>
+    `)
+})
+
+
+http.listen(config.port, () => {
+    console.log('Listening on *:' + config.port);
+});
--- cngmat.js
+++ cngmat.js
@@ -7,17 +7,11 @@ module.exports = function(app, db, config) {
     const { ocrSpace } = require('ocr-space-api-wrapper'); // OCR
 
     Date.prototype.getWeekNumber = function(){var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));var dayNum = d.getUTCDay() || 7;d.setUTCDate(d.getUTCDate() + 4 - dayNum);var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));return Math.ceil((((d - yearStart) / 86400000) + 1)/7)};
-    function bufferToBase64(buf) {
-        var binstr = Array.prototype.map.call(buf, function (ch) {
-            return String.fromCharCode(ch);
-        }).join('');
-        return btoa(binstr);
-    }
 
     let sourceUrl = "https://finspang.se/bergska/bergskagymnasiet/ovriga/matsedel/matsedelibildningen.4.166aa05167c62dff5dc3168.html"
 
     let cngMat = {
-        loadFoodFromSource: () => {
+        loadFoodFromSource: (cb) => {
             const weekReference = {
                 "måndag": "mon",
                 "mandag": "mon",
@@ -41,8 +35,8 @@ module.exports = function(app, db, config) {
                         .toBuffer({ resolveWithObject: true })
                         .then(async ({ data, info }) => {
                             let req = await ocrSpace("data:image/png;base64," + Buffer.from(data).toString('base64'), { apiKey: config.OCRSpaceKey, language: 'swe' });
+                            if(req.IsErroredOnProcessing) return console.error(req.ErrorMessage)
                             let text = req.ParsedResults[0].ParsedText
-                            console.log(text)
                             let weekObject = { // Create an object for this weeks food
                                 year: new Date().getFullYear().toString(),
                                 week: text.replace(/(\D)|(0+(?!$))/gm, "")
@@ -53,6 +47,7 @@ module.exports = function(app, db, config) {
                             .toBuffer({ resolveWithObject: true })
                             .then(async ({ data, info }) => {
                                 let req = await ocrSpace("data:image/png;base64," + Buffer.from(data).toString('base64'), { apiKey: config.OCRSpaceKey, language: 'swe' });
+                                if(req.IsErroredOnProcessing) return console.error(req.ErrorMessage)
                                 let str = (" "+req.ParsedResults[0].ParsedText).replace(/(.|\s)+?(?=Måndag)|/m, "").replace(/\./g, ""); // Remove Weird characters that are not relevant
                                 let days = str.split(/(Måndag|Tisdag|Onsdag|Torsdag|Fredag).*\s/ig).filter(n => n); // Split into each weekday and filter empty
 
@@ -67,8 +62,8 @@ module.exports = function(app, db, config) {
                                         veg: food[2]?food[2]:""
                                     };
                                 }
-                                console.log(weekObject)
                                 db.cngMat.update({$and: [{week: weekObject.week}, {year: weekObject.year}]}, {$setOnInsert: weekObject}, {upsert: true}); // Add available data to database, don't update if data already exists
+                                if(cb) cb(200)
                             });
                         });
                 });
@@ -77,7 +72,7 @@ module.exports = function(app, db, config) {
         getFromDate: function(d, cb) { // Get food, in a callback, from DB by supplying a Date object
             let date = new Date(d);
             let corresponding = [null, "mon", "tue", "wed", "thu", "fri", null]
-            db.cngMat.find({$and: [{week: date.getWeekNumber().toString()}, {year: date.getFullYear().toString()}]}, function(error, docs) {
+            db.cngMat.find({$and: [{week: date.getWeekNumber().toString()}, {year: date.getFullYear().toString()}]}, (error, docs) => {
               if(!corresponding[date.getDay()] || !docs[0]) return cb(404)
               let food = docs[0][corresponding[date.getDay()]]
               food.date = date
@@ -91,14 +86,49 @@ module.exports = function(app, db, config) {
                 currDate.setTime(currDate.getTime() + (24 * 60 * 60 * 1000))
                 today = false;
             }
-            cngMat.getFromDate(currDate, function(food) {
+            cngMat.getFromDate(currDate, (food) => {
                 cb(food, today)
             })
+        },
+
+        getAvailableWeeks: (cb) => { // Get all avaiable weeks in database
+            db.cngMat.distinct("year", {}, async (error, docs) => {
+                let values = {}
+                const promises = docs.map((el) => {
+                    return new Promise((resolve => {
+                        db.cngMat.distinct("week", {year: el}, (error, docs) => {
+                            values[el] = docs;
+                            resolve()
+                        })
+                    }))
+                });
+                // wait until all promises are resolved
+                await Promise.all(promises);
+                cb(values)
+            })
+        },
+        getWeek: (date, cb) => { // Get data of a whole week
+            db.cngMat.find({year: date.year, week: date.week}, (error, docs) => {
+                cb(docs)
+            })
+        },
+
+        admin: {
+            setWeekObject: (weekObj, cb) => { // Update a week with new data, date = {year:2022, week:4}
+                db.cngMat.update({$and:[{week: weekObj.week}, {year: weekObj.year}]}, {$set: weekObj}, (data, err) => {
+                    cb(200)
+                });
+            },
+            deleteWeekObject: (date, cb) => {
+                db.cngMat.remove({$and:[{week: date.week}, {year: date.year}]}, (data, err) => {
+                    cb(200)
+                });
+            }
         }
     }
 
-    cngMat.loadFoodFromSource() // Update database
-    var updateDB = schedule.scheduleJob('00 12 * * *', function(){ // Update DB at 12:00 every day
+    //cngMat.loadFoodFromSource() // Update database
+    var updateDB = schedule.scheduleJob('00 12 * * *', () => { // Update DB at 12:00 every day
         cngMat.loadFoodFromSource()
         console.log("Updated cngMat DB at " + new Date())
     });
